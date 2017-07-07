038a0671dca3c21118d7fe3dd0845d6c1e874925
AntumDeluge <antumdeluge@gmail.com>
2017-07-07 16:02:56 -0700

    Add LGPL license header to source

--- init.lua
+++ init.lua
@@ -1,3 +1,22 @@
+--[[ LICENSE HEADER
+
+  This file is part of boats2 mod for Minetest.
+
+  boats2 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  boats2 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with boats2. If not, see
+  <http://www.gnu.org/licenses/>.
+]]
+
 
 boats2 = {}
 boats2.modname = minetest.get_current_modname()
